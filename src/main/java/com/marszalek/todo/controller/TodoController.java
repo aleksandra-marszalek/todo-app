c5e2ae3d084ed113e69daff720793d9ddc40d819
Aleksandra Marszalek <aleksandramarszalek@macbookpro.home>
2026-02-26 16:59:53 +0000

    Initial commit: Todo API with MySQL and basic CRUD

---
+++ src/main/java/com/marszalek/todo/controller/TodoController.java
@@ -0,0 +1,34 @@
+package com.marszalek.todo.controller;
+
+import com.marszalek.todo.model.Todo;
+import com.marszalek.todo.repository.TodoRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/todos")
+@RequiredArgsConstructor
+public class TodoController {
+
+    private final TodoRepository todoRepository;
+
+    @GetMapping
+    public List<Todo> getAllTodos() {
+        return todoRepository.findAll();
+    }
+
+    @PostMapping
+    public Todo createTodo(@RequestBody Todo todo) {
+        return todoRepository.save(todo);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Todo> getTodoById(@PathVariable Long id) {
+        return todoRepository.findById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+}
---
+++ src/main/java/com/marszalek/todo/repository/TodoRepository.java
@@ -0,0 +1,10 @@
+package com.marszalek.todo.repository;
+
+import com.marszalek.todo.model.Todo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TodoRepository extends JpaRepository<Todo, Long> {
+}
+
